bb7729e507f0489de5a9dc5218da76df52e49e3b
Ivan1874 <ivan51673@gmail.com>
2020-03-12 21:38:20 +0300

    New apps, overrides

--- default-permissions/Android.mk
+++ default-permissions/Android.mk
@@ -1,10 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := phoensky-permissions
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+LOCAL_SRC_FILES := phonesky-permissions.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := default-permissions
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
-LOCAL_SRC_FILES := microg-permissions.xml phonesky-permissions.xml
+LOCAL_SRC_FILES := microg-permissions.xml
+LOCAL_REQUIRED_MODULES := phoensky-permissions
 include $(BUILD_PREBUILT)
-

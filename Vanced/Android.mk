38b3827e45cf7af6f293805c71211b97b2d88ccf
Ivan1874 <ivan51673@gmail.com>
2020-03-13 21:49:30 +0300

    Remove vanced

--- Vanced/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := YouTubeVanced
-LOCAL_SRC_FILES := config.arm64_v8a.apk split_config.af.apk split_config.am.apk split_config.ar.apk split_config.bg.apk split_config.bn.apk split_config.bs.apk split_config.ca.apk split_config.cs.apk split_config.da.apk split_config.de.apk split_config.el.apk split_config.en.apk split_config.es.apk split_config.et.apk split_config.eu.apk split_config.fa.apk split_config.fi.apk split_config.fr.apk split_config.gl.apk split_config.gu.apk split_config.hi.apk split_config.hr.apk split_config.hu.apk split_config.hy.apk split_config.in.apk split_config.is.apk split_config.it.apk split_config.iw.apk split_config.ja.apk split_config.ka.apk split_config.kk.apk split_config.km.apk split_config.kn.apk split_config.ko.apk split_config.lo.apk split_config.lt.apk split_config.lv.apk split_config.mk.apk split_config.ml.apk split_config.mn.apk split_config.mr.apk split_config.ms.apk split_config.my.apk split_config.nb.apk split_config.ne.apk split_config.nl.apk split_config.pa.apk split_config.pl.apk split_config.pt.apk split_config.ro.apk split_config.ru.apk split_config.si.apk split_config.sk.apk split_config.sl.apk split_config.sq.apk split_config.sr.apk split_config.sv.apk split_config.sw.apk split_config.ta.apk split_config.te.apk split_config.th.apk split_config.tl.apk split_config.tr.apk split_config.uk.apk split_config.ur.apk split_config.vi.apk split_config.zh.apk split_config.zu.apk YouTube_15.05.54_API21(nodpi)(vBlack-v2.1.0)-vanced.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)

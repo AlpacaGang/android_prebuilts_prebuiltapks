7914b733f230ebb2a7aec5755d75ed6e191c987e
Ivan1874 <ivan51673@gmail.com>
2020-03-13 21:46:44 +0300

    New apps, fixes

--- default-permissions/Android.mk
+++ default-permissions/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := phoensky-permissions
+LOCAL_MODULE := phoensky-permissions.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
@@ -9,10 +9,19 @@ LOCAL_SRC_FILES := phonesky-permissions.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := default-permissions
+LOCAL_MODULE := google-sync-permissions.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+LOCAL_SRC_FILES := google-sync-permissions.xml
+LOCAL_REQUIRED_MODULES := phoensky-permissions.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := microg-permissions.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
 LOCAL_SRC_FILES := microg-permissions.xml
-LOCAL_REQUIRED_MODULES := phoensky-permissions
+LOCAL_REQUIRED_MODULES := google-sync-permissions.xml
 include $(BUILD_PREBUILT)
